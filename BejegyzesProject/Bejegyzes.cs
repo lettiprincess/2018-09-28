c7dce7e895a6739da975d7675191076e341b7745
Petrik Lajos Szg <info@petrik.hu>
2018-09-28 14:42:28 +0200

    A b feladat kész

--- BejegyzesProject/Bejegyzes.cs
+++ BejegyzesProject/Bejegyzes.cs
@@ -8,11 +8,66 @@ namespace BejegyzesProject
 {
     class Bejegyzes
     {
-        public Bejegyzes()
+        public Bejegyzes(string szerzo, string tartalom)
         {
 
         }
 
+        public string szerzo;
+        public string tartalom;
+        public int lajkok;
+        public DateTime letrejott;
+        public DateTime szerkesztett;
 
+        public string Szerzo
+        {
+            get
+            {
+                return this.szerzo;
+            }
+        }
+
+        public string Tartalom{
+            get
+            {
+                return this.tartalom;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    this.tartalom = value;
+                }
+            }
+        }
+            
+
+        public int Lajkok
+        {
+            get
+            {
+                return this.lajkok;
+            }
+        }
+
+        public DateTime Letrejott
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public DateTime Szerkesztett
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public void Like() {
+             lajkok++;
+        }
     }
 }
